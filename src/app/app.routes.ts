7385656d6d9197d4f66a1adea72b4f4792c318e7
RaBolivar <rachelbolivar01@gmail.com>
2024-10-07 19:40:44 -0600

    Update dashboard component with new html and routes to have dashboard included

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
   { path: 'users', component: UserComponent },
+  { path: 'dashboard', component: DashboardComponent}
 ];
