e0fcdffea1dd9696f3edf01126540e8d5934354c
RaBolivar <rachelbolivar01@gmail.com>
2024-10-07 19:43:11 -0600

    Update user component to match style

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -3,11 +3,12 @@ import { UserService } from '../../services/user.service';
 import { User } from '../../user';
 import { CommonModule } from '@angular/common'; 
 import { Observable } from 'rxjs';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
