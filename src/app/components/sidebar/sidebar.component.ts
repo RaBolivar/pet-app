7b76eab66d7d56bad4dcf59d8581b45b02545db6
RaBolivar <rachelbolivar01@gmail.com>
2024-10-07 19:42:28 -0600

    Update sidebar html and ts

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { RouteInfo } from '../../route-info';
+import { CommonModule } from '@angular/common';
+
+export const ADMIN_ROUTES: RouteInfo[] = [
+  { path: 'dashboard', title: 'Dashboard', icon: 'fa-solid fa-calendar text-blue', class: '' },
+  { path: 'users', title: 'Users', icon: 'fa-solid fa-user text-blue', class: '' },
+];
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  public menuItems: any[] = [];
+  public isCollapsed = true;
 
+  constructor(private router: Router){}
+
+  ngOnInit() {
+    this.menuItems = ADMIN_ROUTES.filter(menuItem => menuItem);
+
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+    });
+  }
 }
