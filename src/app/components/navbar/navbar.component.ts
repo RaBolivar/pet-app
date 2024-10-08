fcf2df403f085b403b095f538cbace786cc2b0f4
RaBolivar <rachelbolivar01@gmail.com>
2024-10-07 19:42:05 -0600

    Update navbar html and ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule, Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  public focus: any = null;
+  public userName = "Rachel B";
 }
